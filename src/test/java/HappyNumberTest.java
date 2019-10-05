52618672d8ff97396957496f33e7bb8bd0b9cf09
Sush25b <phulsundarsushant26@gmail.com>
2019-10-05 18:54:50 +0530

    [Sushant] Add. give One then It is Happy Number

--- src/test/java/HappyNumberTest.java
+++ src/test/java/HappyNumberTest.java
@@ -7,5 +7,9 @@ import static org.junit.Assert.*;
 
 public class HappyNumberTest {
 
-
+    @Test
+    public void givenNumberOne_whenCheck_thenNumberIsAHappyNumber() {
+        HappyNumber happyNumber = new HappyNumber(1);
+        Assert.assertTrue(happyNumber.checkNumber());
+    }
 }
